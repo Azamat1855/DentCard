6386be85a1e9c2d380d5148aa407724d2474c5f7
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-07-21 15:41:57 +0500

    ASD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,15 @@ import { AiFillMedicineBox } from 'react-icons/ai';
 
 const Navbar = ({ medCards, onSelect }) => {
   return (
-    <div className="w-full bg-gray-100 py-[0] shadow-md shadow-slate-300">
-      <div className="w-[80%] mx-auto flex justify-between items-center">
+    <div className="w-full bg-gray-100 py-4 shadow-md shadow-slate-300">
+      <div className="w-[80%] mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center">
-          <AiFillMedicineBox className="text-[70px] text-red-500" />
-          <h1 className="font-bold text-2xl ml-2">Medical Cards</h1>
+          <AiFillMedicineBox className="text-[50px] md:text-[70px] text-red-500" />
+          <h1 className="font-bold text-xl md:text-2xl ml-2">Medical Cards</h1>
         </div>
-        <MedCardDropdown medCards={medCards} onSelect={onSelect} />
+        <div className="mt-4 md:mt-0">
+          <MedCardDropdown medCards={medCards} onSelect={onSelect} />
+        </div>
       </div>
     </div>
   );
